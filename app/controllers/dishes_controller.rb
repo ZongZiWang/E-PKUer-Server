702b5634fcc28d34e247f1a580f10654da0d76cb
WangZhongyu <wzy.dtc@gmail.com>
2012-12-28 23:20:31 +0800

    change nested controller for dishes

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -1,8 +1,11 @@
 class DishesController < ApplicationController
+	
+  before_filter :load_restaurant
+	
   # GET /dishes
   # GET /dishes.json
   def index
-    @dishes = Dish.all
+    @dishes = @restaurant.dishes.all
 
     respond_to do |format|
       format.html # index.html.erb
@@ -13,7 +16,7 @@ class DishesController < ApplicationController
   # GET /dishes/1
   # GET /dishes/1.json
   def show
-    @dish = Dish.find(params[:id])
+    @dish = @restaurant.dishes.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
@@ -24,7 +27,7 @@ class DishesController < ApplicationController
   # GET /dishes/new
   # GET /dishes/new.json
   def new
-    @dish = Dish.new
+    @dish = @restaurant.dishes.new
 
     respond_to do |format|
       format.html # new.html.erb
@@ -34,17 +37,17 @@ class DishesController < ApplicationController
 
   # GET /dishes/1/edit
   def edit
-    @dish = Dish.find(params[:id])
+    @dish = @restaurant.dishes.find(params[:id])
   end
 
   # POST /dishes
   # POST /dishes.json
   def create
-    @dish = Dish.new(params[:dish])
+    @dish = @restaurant.dishes.new(params[:dish])
 
     respond_to do |format|
       if @dish.save
-        format.html { redirect_to @dish, notice: 'Dish was successfully created.' }
+        format.html { redirect_to [@restaurant, @dish], notice: 'Dish was successfully created.' }
         format.json { render json: @dish, status: :created, location: @dish }
       else
         format.html { render action: "new" }
@@ -56,11 +59,11 @@ class DishesController < ApplicationController
   # PUT /dishes/1
   # PUT /dishes/1.json
   def update
-    @dish = Dish.find(params[:id])
+    @dish = @restaurant.dishes.find(params[:id])
 
     respond_to do |format|
       if @dish.update_attributes(params[:dish])
-        format.html { redirect_to @dish, notice: 'Dish was successfully updated.' }
+        format.html { redirect_to [@restaurant, @dish], notice: 'Dish was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
@@ -72,12 +75,19 @@ class DishesController < ApplicationController
   # DELETE /dishes/1
   # DELETE /dishes/1.json
   def destroy
-    @dish = Dish.find(params[:id])
+    @dish = @restaurant.dishes.find(params[:id])
     @dish.destroy
 
     respond_to do |format|
-      format.html { redirect_to dishes_url }
+      format.html { redirect_to restaurant_dishes_url(@restaurant) }
       format.json { head :no_content }
     end
   end
+
+  private
+    
+  def load_restaurant
+	  @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
 end
