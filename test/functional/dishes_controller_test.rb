6b5a431567b1a7706049023c2d53040cd8ef2e24
WangZhongyu <wzy.dtc@gmail.com>
2012-12-29 15:55:59 +0800

    fix all bugs for now

--- test/functional/dishes_controller_test.rb
+++ test/functional/dishes_controller_test.rb
@@ -15,44 +15,44 @@ class DishesControllerTest < ActionController::TestCase
   end
 
   test "should get index" do
-    get :index
+    get :index, restaurant_id: @dish.restaurant_id
     assert_response :success
     assert_not_nil assigns(:dishes)
   end
 
   test "should get new" do
-    get :new
+    get :new, restaurant_id: @dish.restaurant_id
     assert_response :success
   end
 
   test "should create dish" do
     assert_difference('Dish.count') do
-      post :create, dish: @update 
+      post :create, restaurant_id: @dish.restaurant_id, dish: @update 
     end
 
-    assert_redirected_to dish_path(assigns(:dish))
+    assert_redirected_to restaurant_dish_path(assigns(:restaurant), assigns(:dish))
   end
 
   test "should show dish" do
-    get :show, id: @dish
+    get :show, restaurant_id: @dish.restaurant_id, id: @dish
     assert_response :success
   end
 
   test "should get edit" do
-    get :edit, id: @dish
+    get :edit, restaurant_id: @dish.restaurant_id, id: @dish
     assert_response :success
   end
 
   test "should update dish" do
-    put :update, id: @dish.to_param, dish: @update
-    assert_redirected_to dish_path(assigns(:dish))
+    put :update, restaurant_id: @dish.restaurant_id, id: @dish.to_param, dish: @update
+    assert_redirected_to restaurant_dish_path(assigns(:restaurant), assigns(:dish))
   end
 
   test "should destroy dish" do
     assert_difference('Dish.count', -1) do
-      delete :destroy, id: @dish
+      delete :destroy, restaurant_id: @dish.restaurant_id, id: @dish
     end
 
-    assert_redirected_to dishes_path
+    assert_redirected_to restaurant_dishes_path(assigns(:restaurant))
   end
 end
